09620b92fcb804d76125aa7ddd853c6773396fcc
sabravo <belloaugustine@gmail.com>
2022-12-08 22:08:55 +0000

    a function that returns the number of elements in a linked dlistint_t list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,25 +1,20 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- * @head: pointer to the first element of the list.
- * @n: int to set in the new node.
- * Return: address of the new element, or NULL if it failed
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
  **/
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+size_t dlistint_len(const dlistint_t *h)
 {
-	dlistint_t *new;
+	const dlistint_t *node = h;
+	size_t cont = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
 
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	if (*head)
-		(*head)->prev = new;
-	*head = new;
-
-	return (new);
+	return (cont);
 }
