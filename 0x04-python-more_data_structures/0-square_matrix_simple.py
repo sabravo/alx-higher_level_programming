921791759cc16bbe6c04645520d947751ef30069
sabravo <belloaugustine@gmail.com>
2022-12-08 00:47:48 +0000

    a function that computes the square value of all integers of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
-import random
-
-number = random.randint(-10, 10)
-if number > 0:
-    print(f"{number:d} is positive")
-elif number == 0:
-    print(f"{number:d} is zero")
-else:
-    print(f"{number:d} is negative")
+def square_matrix_simple(matrix=[]):
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
