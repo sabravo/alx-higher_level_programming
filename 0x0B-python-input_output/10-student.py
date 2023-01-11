c37706d1b9e182ee7d1710bec0abe38d7e520ffb
sabravo <belloaugustine@gmail.com>
2023-01-11 04:26:57 +0000

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-
-"""Defines a Student class"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """A Student class"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student object
-        Args:
-            first_name (str): first name of the student
-            last_name (str): last name of the student
-            age (int): age of the student
+        """Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student
-        If attrs is a list of strings, only attribute names
-        contained in this list are retrieved.
-        Args:
-            attrs (list): attributes to retrieve
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
         """
-        if type(attrs) == list:
-            if all(type(elem) == str for elem in attrs):
-                return {key: getattr(self, key)
-                        for key in attrs if hasattr(self, key)}
-        return self.
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
